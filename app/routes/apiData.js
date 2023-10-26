7a4196e7474e892b77936cce0b706fe342aa3dab
Lucas <medinalucasian@gmail.com>
2023-10-26 17:24:35 -0300

    update endpoints

--- app/routes/apiData.js
+++ app/routes/apiData.js
@@ -1,5 +1,23 @@
 const express=require('express');
 const router=express.Router();
+const apiSchema= require('../models/apiSchema')
 
+router.post('/apick', (req,res)=>{
+    const api=apiSchema(req.body);
+    api
+        .save()
+        .then((data)=>res.json(data))
+        .catch((err)=>res.json({message:err}))
+})
+
+
+
+router.get('/apick', (req, res) => {
+    apiSchema
+    .find({})
+    .then((data)=>{
+        res.json(data)
+    })
+});
 
 module.exports= router;
